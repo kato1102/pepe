15c07b64e6bbab6e78a682a5590906c3f062cbfb
kato1102 <167833150+kato1102@users.noreply.github.com>
2025-01-13 21:48:18 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -2,6 +2,6 @@ function imgchange1() {
     document.getElementById('BABY').src = './image/2.jpeg';
 }
 function imgchange2() {
-    document.getElementById('BABY').src = './image/1.jpeg';
+    document.getElementById('BABY').src = './image/1.jpg';
 }
 
